7510974a7bed33f1c5384edf2fa0d25c3dc24f5a
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2022-07-14 11:01:43 -0400

    Added return to shopping button on order confirmation page

--- view/src/components/orders/OrderInfo.js
+++ view/src/components/orders/OrderInfo.js
@@ -1,16 +1,24 @@
 import { OrderShipping } from "./OrderShipping"
 import currency from 'currency.js';
+import { useNavigate } from 'react-router-dom';
 
 export const OrderInfo = ({total, address}) => {
 
+    const navigate = useNavigate();
+
     const displayTotal = () => {
         return currency(total/100).format();
+    };
+
+    const returnBtn = () => {
+        navigate('/homepage')
     }
 
     return(
         <div className="order-info">
             <OrderShipping address={address} />
             <p className="order-total">Total: {displayTotal()}</p>
+            <button className="submit-btn" onClick={returnBtn}>Back to shopping</button>
         </div>
     )
 }
